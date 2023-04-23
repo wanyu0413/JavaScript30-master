f6e2f9d048b18e89c141f7722a9a59fed08a5fc3
Wanyu Jiang <wanyu19930930@gmail.com>
2023-04-23 23:33:22 +0900

    update

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,30 @@
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+function togglePlay() {
+  // if(video.paused){
+  //   video.play();
+  // }else{
+  //   video.pause();
+  // }
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  console.log('Update the button')
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
